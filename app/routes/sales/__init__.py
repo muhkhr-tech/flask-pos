4f4aa2c0b1cf1a6ca48c53dffafeb6e48ba8644b
rafiq <muhkhr-tech@gmail.com>
2025-01-21 22:51:08 +0800

    refactor(query): pindahkan query ORM ke PostgreSQL fungsi untuk efesiensi

--- app/routes/sales/__init__.py
+++ app/routes/sales/__init__.py
@@ -1,9 +1,7 @@
-from flask import Blueprint, render_template, request, redirect, url_for, current_app
+from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import current_user, login_required
-from sqlalchemy.orm import joinedload
 
-from app.core.db import get_db_session
-from app import crud, schemas, models
+from app import crud
 from app.utils import role_required
 
 sale_bp = Blueprint("sales", __name__, template_folder="templates")
@@ -13,9 +11,7 @@ sale_bp = Blueprint("sales", __name__, template_folder="templates")
 @login_required
 @role_required("cashier")
 def index():
-    with get_db_session() as db:
-        rows = db.query(models.Sale).options(joinedload(models.Sale.cashier)).all()
-
+    rows = crud.get_all_sales()
     return render_template("sales/index.html", rows=rows)
 
 
@@ -23,14 +19,13 @@ def index():
 async def create_sale():
     user_id = current_user.user_id
     product_id = request.form["product_id"]
-    price = request.form["price"]
 
     get_sale_await = await crud.get_sale("menunggu")
 
     if get_sale_await:
-        await crud.update_sale(user_id, get_sale_await.sale_id, product_id, price)
+        await crud.update_cart(user_id, get_sale_await.sale_id, product_id)
     else:
-        await crud.create_sale(user_id, product_id, price)
+        await crud.create_sale(user_id, product_id)
     return redirect(url_for("main.index"))
 
 
